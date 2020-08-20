44b366300b2044a9942a531402553abc9e78ef63
Sergio Yonashiro <sergio.yonashiro@outlook.com>
2020-08-19 22:17:42 -0300

    Implementa endpoints

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { uuid } = require('uuidv4');
 
 const app = express();
 
@@ -11,23 +11,51 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const object = { 
+    id: uuid(), 
+    title: request.body.title, 
+    url: request.body.url, 
+    techs: request.body.techs, 
+    likes: 0 
+  }
+
+  repositories.push(object);
+
+  return response.json(object);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const index = repositories.findIndex(repo => repo.id === id);
+  const repo = { ...repositories[index], title, url, techs, };
+
+  repositories[index] = repo;
+
+  return response.json(repo);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const index = repositories.findIndex(repo => repo.id === id);
+
+  repositories.splice(index, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const index = repositories.findIndex(repo => repo.id === id);
+
+  repositories[index].likes++;
+
+  return response.json(repositories[index]);
 });
 
 module.exports = app;
